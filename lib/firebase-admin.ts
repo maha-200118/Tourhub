7943b74b830caf02b174199bc7940861cb1b5295
maha-200118 <mahamurugan1810@gmail.com>
2026-03-04 23:36:05 +0530

    fix: use env vars for firebase-admin instead of service account file

--- lib/firebase-admin.ts
+++ lib/firebase-admin.ts
@@ -2,10 +2,13 @@ import * as admin from 'firebase-admin';
 
 if (!admin.apps.length) {
   const serviceAccount = require('../lib/service-account.json');
-  admin.initializeApp({
-    credential: admin.credential.cert(serviceAccount),
-  });
+ admin.credential.cert({
+      projectId: process.env.FIREBASE_PROJECT_ID,
+      clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+      privateKey: process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, '\n'),
+    }),
 }
+
 
 export default admin;
 
