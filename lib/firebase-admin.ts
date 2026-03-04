988bc9c448206fd6e2c7b359df71fe9c5b5e5203
maha-200118 <mahamurugan1810@gmail.com>
2026-03-05 00:24:26 +0530

    fix: firebase-admin privateKey syntax error

--- lib/firebase-admin.ts
+++ lib/firebase-admin.ts
@@ -1,15 +1,15 @@
 import * as admin from 'firebase-admin';
 
 if (!admin.apps.length) {
-  const serviceAccount = require('../lib/service-account.json');
- admin.credential.cert({
+  admin.initializeApp({
+    credential: admin.credential.cert({
       projectId: process.env.FIREBASE_PROJECT_ID,
       clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
-      privateKey: process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, '\n'),
+      privateKey: process.env.FIREBASE_PRIVATE_KEY
+        ? process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n')
+        : undefined,
     }),
+  });
 }
 
-
 export default admin;
-
-
